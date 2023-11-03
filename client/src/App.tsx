7e0d30a47eae9f78d6f86820feaf04b2bf0f0016
Micheal Pearce <michealpearce@outlook.com>
2023-11-03 00:54:39 -0400

    add logging for store imports in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { StoreProvider } from '@client/utils/createStore';
 import { Routes } from '@generouted/react-router';
+import { toEntries } from '@sa-net/utils';
 
 const storesImports = import.meta.glob<{
 	default?: StoreProvider<any>;
@@ -8,8 +9,10 @@ const storesImports = import.meta.glob<{
 });
 
 const storeContexts = new Set<StoreProvider<any>>();
-for (const storeImport of Object.values(storesImports)) {
+for (const [importPath, storeImport] of toEntries(storesImports)) {
+	console.info(`Loading store ${importPath}`);
 	if (storeImport.default) storeContexts.add(storeImport.default);
+	else console.warn(`No default export found for ${importPath}`);
 }
 
 export function App() {
